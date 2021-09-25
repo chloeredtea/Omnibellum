b79618b25746944765c1b061c6dade985a8f856d
Chloe <chloeredtea@gmail.com>
2021-09-25 16:17:53 -0500

    app working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var path = require('path');
+var app = express();
 
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-server.listen(process.env.PORT || 3000);
+app.listen(3000);
