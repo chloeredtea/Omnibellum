eb126282bf8acfed7569e403eb11430c1f5c16f1
Chloe <chloeredtea@gmail.com>
2021-09-25 20:20:32 -0500

    vertex bugfix

--- public/mapeditor/mapeditor.js
+++ public/mapeditor/mapeditor.js
@@ -1,7 +1,5 @@
 
 // Loading uploaded image into memory
-
-// Black box -- DO NOT TOUCH
 window.addEventListener('load', function() {
     document.querySelector('input[type="file"]').addEventListener('change', function() {
         if (this.files && this.files[0]) {
@@ -14,12 +12,10 @@ window.addEventListener('load', function() {
     });
   });
 
-
 // Putting image onto canvas
 
 let imgData, c;
-
-
+let vertexdata = {};
 
 function GenerateBorders(){
     c = document.getElementById("canvas1");
@@ -30,15 +26,25 @@ function GenerateBorders(){
     ctx.drawImage(img, 0, 0)
 
 // Finding the borders of the states and putting them into two dictionaries.
-    
-    
     imgRawData = ctx.getImageData(0, 0, c.width, c.height);
     imgData = imgRawData.data;
     for(let i = 0; i < imgData.length; i+=4){
+        let initialcolor = "" + imgData[i]+imgData[i+1]+imgData[i+2];
         if(imgData[i] != 0 || imgData[i+1] != 0 || imgData[i+2] != 0){
             // Check Right
             if(i < imgData.length - 4 && (imgData[i] != imgData[i+4] || imgData[i+1] != imgData[i+5] || imgData[i+2] != imgData[i+6])){
                 if(imgData[i+4] != 0 || imgData[i+5] != 0 || imgData[i+6] != 0){
+                    if("" + imgData[i]+imgData[i+1]+imgData[i+2] != "255255255"){
+                        if(vertexdata.hasOwnProperty(initialcolor)){
+                            vertexdata[initialcolor][0].push(i/4 - c.width*Math.floor((i/4)/c.width));
+                            vertexdata[initialcolor][1].push(Math.floor((i/4)/c.width));
+                        }
+                        else{
+                            vertexdata[initialcolor] = [[], []];
+                            vertexdata[initialcolor][0].push(i/4 - c.width*Math.floor((i/4)/c.width));
+                            vertexdata[initialcolor][1].push(Math.floor((i/4)/c.width));
+                        }
+                    }
                     imgData[i] = 0;
                     imgData[i+1] = 0;
                     imgData[i+2] = 0;
@@ -46,15 +52,46 @@ function GenerateBorders(){
                 }
             }
             // Check Up
-            if(i > c.width*4 && (imgData[i] != imgData[i-c.width*4] || imgData[i+1] != imgData[i-c.width*4+1] || imgData[i+2] != imgData[i-c.width*4+2])){
-                
+            if(i > c.width*4 && (imgData[i-c.width*4] == 0 && imgData[i-c.width*4+1] == 0 && imgData[i-c.width*4+2] == 0)){
+                if(initialcolor != "255255255"){
+                    if(vertexdata.hasOwnProperty(initialcolor)){
+                        vertexdata[initialcolor][0].push((i-c.width*4)/4 - c.width*Math.floor(((i-c.width*4)/4)/c.width));
+                        vertexdata[initialcolor][1].push(Math.floor(((i-c.width*4)/4)/c.width));
+                    }
+                    else{
+                        vertexdata[initialcolor] = [[], []];
+                        vertexdata[initialcolor][0].push((i-c.width*4)/4 - c.width*Math.floor(((i-c.width*4)/4)/c.width));
+                        vertexdata[initialcolor][1].push(Math.floor(((i-c.width*4)/4)/c.width));
+                    }
+                }
             }
             // Check left
-            if(i > 0 && (imgData[i-4] != imgData[i] || imgData[i-3] != imgData[i+1] || imgData[i-2] != imgData[i+2])){
-                
+            if(i > 0 && (imgData[i-4] == 0 && imgData[i-3] == 0 && imgData[i-2] == 0)){
+                if(initialcolor != "255255255"){
+                    if(vertexdata.hasOwnProperty(initialcolor)){
+                        vertexdata[initialcolor][0].push((i-4)/4 - c.width*Math.floor(((i-4)/4)/c.width));
+                        vertexdata[initialcolor][1].push(Math.floor(((i-4)/4)/c.width));
+                    }
+                    else{
+                        vertexdata[initialcolor] = [[], []];
+                        vertexdata[initialcolor][0].push((i-4)/4 - c.width*Math.floor(((i-4)/4)/c.width));
+                        vertexdata[initialcolor][1].push(Math.floor(((i-4)/4)/c.width));
+                    }
+                }
             }
             // Check Down
             if(i < imgData.length - c.width*4 && (imgData[i] != imgData[i+c.width*4] || imgData[i+1] != imgData[i+1+c.width*4] || imgData[i+2] != imgData[i+2+c.width*4])){
+                if(initialcolor != "255255255"){
+                    if(vertexdata.hasOwnProperty(initialcolor)){
+                        vertexdata[initialcolor][0].push(i/4 - c.width*Math.floor((i/4)/c.width));
+                        vertexdata[initialcolor][1].push(Math.floor((i/4)/c.width));
+                    }
+                    else{
+                        vertexdata[initialcolor] = [[], []];
+                        vertexdata[initialcolor][0].push(i/4 - c.width*Math.floor((i/4)/c.width));
+                        vertexdata[initialcolor][1].push(Math.floor((i/4)/c.width));
+                    }
+                }
                 imgData[i] = 0;
                 imgData[i+1] = 0;
                 imgData[i+2] = 0;
@@ -67,9 +104,7 @@ function GenerateBorders(){
             imgData[i+2] = 0;
             imgData[i+3] = 0;
         }
-        
     }
-    
     let c2 = document.getElementById("canvas2");
     let ctx2 = c2.getContext("2d");
     c2.width = c.width;
