318a202b3b954ed1a065447c31bb232be62b4af9
Chloe <chloeredtea@gmail.com>
2021-09-27 17:30:16 -0500

    basic html/css structure

--- public/client.js
+++ public/client.js
@@ -3,18 +3,26 @@ const dom = {
     c: document.getElementById("canvas")
 }
 const ctx = dom.c.getContext("2d")
-const socket = io();
+let socket;
 let img;
-
-socket.on("message", (value) =>{
-    console.log(value);
-    img = new Image();
-    img.src = "data:image/png;base64," + value;
-});
 
 // Global Functions
 function Init(){
+    socket = io();
+    InitSocketFunctions();
     let game = new Game();
+    dom.c.width = 800;
+    dom.c.height = 600;
+    ctx.fillRect(0, 0, 800, 600);
+}
+
+function InitSocketFunctions(){
+    // Recieving Images
+    /*socket.on("message", (value) =>{
+        console.log(value);
+        img = new Image();
+        img.src = "data:image/png;base64," + value;
+    });*/
 }
 
 
